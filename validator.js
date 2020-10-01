1e90ae627482704e3a84b730334b32c5e8bcae39
Kamlesh Mistry <mistrykamlesh312@gmail.com>
2020-10-01 09:52:32 +0530

    Email validator regex

--- validator.js
+++ validator.js
@@ -1,12 +1,9 @@
 function ev(){
-	var x=document.form1.em.value;
-	a=x.length;
-	var atpos=x.indexOf('@')
-	var dotpos=x.indexOf('.')
-	if(atpos<1||dotpos<atpos+1||dotpos+1>=x.length){
-		alert("Enter Valid Email");
-		return false;
-	}
+ 	if (!(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/).test(myForm.emailAddr.value))
+  	{	
+    		alert("You have entered an invalid email address!")
+    		return (false)
+  	}
 }
 function pv(){
 	var y=document.form1.pn.value;
